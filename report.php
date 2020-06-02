90310fe0a13d400c221cb3dd30e7d818ebdaecb4
Spike <hualterio@gmail.com>
2020-06-02 19:45:01 -0300

    evitar el autocomplete en Fechas del report.php

--- report.php
+++ report.php
@@ -13,13 +13,13 @@
 				  <div class="form-group">
 				    <label for="startDate" class="col-sm-2 control-label">Fecha inicial</label>
 				    <div class="col-sm-10">
-				      <input type="text" class="form-control" id="startDate" name="startDate" placeholder="Fecha inicial" />
+				      <input type="text" class="form-control" id="startDate" name="startDate" autocomplete="off" placeholder="Fecha inicial" />
 				    </div>
 				  </div>
 				  <div class="form-group">
 				    <label for="endDate" class="col-sm-2 control-label">Fecha final</label>
 				    <div class="col-sm-10">
-				      <input type="text" class="form-control" id="endDate" name="endDate" placeholder="Fecha final" />
+				      <input type="text" class="form-control" id="endDate" name="endDate" autocomplete="off" placeholder="Fecha final" />
 				    </div>
 				  </div>
 				  <div class="form-group">
